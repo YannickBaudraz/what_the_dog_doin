78401d456f7834e33975cbbcf0f25e8ece1ecab5
thynkon <thynkon@protonmail.com>
2022-01-18 21:59:46 +0000

    Display order by dropdown in breeds homepage

--- app/app/src/main/java/com/example/whatthedogdoin/ui/breeds/BreedsFragment.kt
+++ app/app/src/main/java/com/example/whatthedogdoin/ui/breeds/BreedsFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.whatthedogdoin.databinding.FragmentBreedsBinding
+import com.example.whatthedogdoin.R;
 
 class BreedsFragment : Fragment() {
 
@@ -19,6 +19,14 @@ class BreedsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onResume() {
+        super.onResume()
+
+        val orderBy = resources.getStringArray(R.array.breeds_sort);
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, orderBy);
+        binding.autoCompleteTextView.setAdapter(arrayAdapter);
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -28,12 +36,9 @@ class BreedsFragment : Fragment() {
                 ViewModelProvider(this).get(BreedsViewModel::class.java)
 
         _binding = FragmentBreedsBinding.inflate(inflater, container, false)
+
         val root: View = binding.root
 
-        val textView: TextView = binding.textRaces
-        breedsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
