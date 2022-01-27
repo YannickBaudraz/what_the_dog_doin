a9c7e0b843baf8a05e7c2191af390f74cad056af
thynkon <thynkon@protonmail.com>
2022-01-27 19:53:43 +0100

    Add floating button to navigation bar
    For some reason, when I tried to a fragment to activity_main.xml,
    the application crashed. It gave me the following error: 'Error inflating class fragment'.
    
    Since the floagin button is in our mockups, I decided to still add it to git but not in
    the master branch so our teacher can still evaluate what we have done.

--- app/src/main/java/com/example/whatthedogdoin/MainActivity.kt
+++ app/src/main/java/com/example/whatthedogdoin/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.get
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.whatthedogdoin.databinding.ActivityMainBinding
+import com.google.android.material.internal.NavigationMenu
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,16 +22,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        setContentView(R.layout.activity_main)
 
-        /*val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dogs, R.id.navigation_breeds,R.id.navigation_diseases))
-        setupActionBarWithNavController(navController, appBarConfiguration) // */
-        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
 
         // Set dark mode as default
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
