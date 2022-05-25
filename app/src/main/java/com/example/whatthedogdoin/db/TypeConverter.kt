2ab039bed3fa0157b1bb64fb2b6f7dadb93f522c
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-05-25 14:37:52 +0200

    Add TypeConverter

---
+++ app/src/main/java/com/example/whatthedogdoin/db/TypeConverter.kt
@@ -0,0 +1,29 @@
+package com.example.whatthedogdoin.db
+
+import java.util.*
+import androidx.room.TypeConverter
+import java.text.SimpleDateFormat
+
+class TypeConverter {
+    private val format: SimpleDateFormat = SimpleDateFormat("YYYY-MM-DD")
+
+    @TypeConverter
+    fun fromString(value: String?): Date? {
+        return format.parse(value)
+    }
+
+    @TypeConverter
+    fun dateToString(date: Date?): String? {
+        return format.format(date)
+    }
+
+    @TypeConverter
+    fun fromInteger(value: Int?): Boolean? {
+        return value == 1;
+    }
+
+    @TypeConverter
+    fun booleanToInteger(boolean: Boolean?): Int? {
+        return if(boolean == true) 1 else 0
+    } // */
+}
